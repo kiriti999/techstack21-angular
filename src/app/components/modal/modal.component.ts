ccb9b6d9fd6ec12bfb416c6a491029b01c2e9bc7
Kiriti K <kiriti.k999@gmail.com>
2018-11-17 00:46:35 +0530

    add dummy modal

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
+  button: any;
   constructor() { }
 
   ngOnInit() {
+    this.button = document.querySelector(`button[data-modal-trigger]`);
   }
+
+  modalEvent(e) {
+      const trigger = e.target.getAttribute('data-modal-trigger');
+      const modal = document.querySelector(`[data-modal=${trigger}]`);
+      const contentWrapper = modal.querySelector('.content-wrapper');
+      const close = modal.querySelector('.close');
+
+      close.addEventListener('click', () => modal.classList.remove('open'));
+      modal.addEventListener('click', () => modal.classList.remove('open'));
+      contentWrapper.addEventListener('click', (e) => e.stopPropagation());
+      modal.classList.toggle('open');
+  }
+
 
 }
