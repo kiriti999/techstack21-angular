9f3d55d34003974912961d6f2bd8d8eca61a7092
Kiriti K <kiriti.k999@gmail.com>
2018-11-17 12:17:29 +0530

    m

--- src/app/components/center/center.component.ts
+++ src/app/components/center/center.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-center',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CenterComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  modalEvent(e) {
+    const trigger = e.target.getAttribute('data-modal-trigger');
+    const modal = document.querySelector(`[data-modal=${trigger}]`);
+    const contentWrapper = modal.querySelector('.content-wrapper');
+    const close = modal.querySelector('.close');
+
+    close.addEventListener('click', () => modal.classList.remove('open'));
+    modal.addEventListener('click', () => modal.classList.remove('open'));
+    contentWrapper.addEventListener('click', (e) => e.stopPropagation());
+    modal.classList.toggle('open');
+}
 
 }
