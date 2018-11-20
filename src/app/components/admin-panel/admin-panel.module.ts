76ea9b77a294d925ac2c67cd4d36a0279ad7082f
Kiriti K <kiriti.k999@gmail.com>
2018-11-20 10:24:08 +0530

    admin routng

--- src/app/components/admin-panel/admin-panel.module.ts
+++ src/app/components/admin-panel/admin-panel.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AdminPanelRoutingModule } from './admin-panel-routing.module';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AdminPanelRoutingModule
   ],
   declarations: []
 })
